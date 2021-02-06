4842a1e8af3bcafc7f51a958003fe37b68fce524
kriswep <kriswep@wetainment.com>
2021-02-06 21:52:36 +0100

    enable hmr + fast-refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import React, { useState, useEffect } from 'react';
 
-import React from "react";
+const App = (props) => {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1 className="text-4xl text-white bg-black">
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
-}
+  const { name } = props;
+  return (
+    <div className="p-2">
+      <h1 className="text-4xl p-2 rounded-md shadow-md text-white bg-black mb-5">
+        Hello {name}
+      </h1>
+      <p className="text-xl p-2 rounded-md shadow-md text-green-200 bg-green-800">
+        Opened for <code>{count}</code> seconds.
+      </p>
+    </div>
+  );
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
 
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+var mountNode = document.getElementById('app');
+ReactDOM.render(
+  <React.StrictMode>
+    <App name="Jane" />
+  </React.StrictMode>,
+  mountNode,
+);
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
