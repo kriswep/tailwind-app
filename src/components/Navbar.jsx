f077b2090d2a73be94d6a44901876ecb093b8ed6
kriswep <kriswep@wetainment.com>
2021-02-13 14:55:31 +0100

    reuse NavItem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
-  let location = useLocation();
+const NavItem = ({ to, text, first }) => {
+  const location = useLocation();
 
   return (
-    <nav className="flex items-center justify-between px-4 text-gray-900 align-middle bg-gray-100 text-l">
+    <Link
+      to={to}
+      className={`px-6 py-6 transition-colors duration-300 transform -skew-x-12  border-r border-yellow-800 hover:text-gray-900 hover:bg-yellow-400 ${
+        location.pathname === to ? 'bg-yellow-300' : ''
+      } ${first ? 'border-l' : ''}`}
+    >
+      <span className="inline-block transform skew-x-12">{text}</span>
+    </Link>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between px-4 space-x-4 text-gray-900 align-middle bg-gray-100 text-l">
       {/* Logo */}
       <Link
         to="/"
@@ -20,9 +33,9 @@ const Navbar = () => {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
               d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
             />
           </svg>
@@ -30,36 +43,15 @@ const Navbar = () => {
         Tailwind-app
       </Link>
       {/* Primary */}
-      <div className="flex border-collapse">
-        <Link
-          to="/features"
-          className={`${
-            location.pathname === '/features' ? 'bg-yellow-300' : ''
-          } inline-block px-6 py-6 transition-colors duration-300 transform -skew-x-12 border-l border-r border-yellow-800 hover:text-gray-900 hover:bg-yellow-400`}
-        >
-          <span className="inline-block transform skew-x-12">Features</span>
-        </Link>
-        <a
-          href="#"
-          className="px-6 py-6 transition-colors duration-300 transform -skew-x-12 border-r border-yellow-800 hover:text-gray-900 hover:bg-yellow-400"
-        >
-          <span className="inline-block transform skew-x-12">Pricing</span>
-        </a>
+      <div className="flex">
+        <NavItem to="/features" text="Features" first />
+        <NavItem to="/pricing" text="Pricing" />
       </div>
       {/* Secondary */}
+
       <div className="flex">
-        <a
-          href="#"
-          className="px-6 py-6 transition-colors duration-300 transform -skew-x-12 border-l border-r border-yellow-800 hover:text-gray-900 hover:bg-yellow-400"
-        >
-          <span className="inline-block transform skew-x-12">Sign In</span>
-        </a>
-        <a
-          href="#"
-          className="px-6 py-6 transition-colors duration-300 transform -skew-x-12 border-r border-yellow-800 hover:text-gray-900 hover:bg-yellow-400"
-        >
-          <span className="inline-block transform skew-x-12">Sign Up</span>
-        </a>
+        <NavItem to="/signin" text="Sign In" first />
+        <NavItem to="/signup" text="Sign up" />
       </div>
     </nav>
   );
